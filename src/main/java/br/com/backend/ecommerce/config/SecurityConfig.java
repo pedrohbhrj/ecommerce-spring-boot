6b12342b100fce1c5d2a90b5fae521fbcd68182e
pedro <l2pedro157@gmail.com>
2026-04-13 14:11:17 -0300

    add: metodos de encode password e autentication manager config

--- src/main/java/br/com/backend/ecommerce/config/SecurityConfig.java
+++ src/main/java/br/com/backend/ecommerce/config/SecurityConfig.java
@@ -1,15 +1,22 @@
 package br.com.backend.ecommerce.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security){
         return security
                 .csrf(AbstractHttpConfigurer::disable)
@@ -19,4 +26,15 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .build();
     }
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authentication(AuthenticationConfiguration authConfiguration){
+        return authConfiguration.getAuthenticationManager();
+    }
+
 }
