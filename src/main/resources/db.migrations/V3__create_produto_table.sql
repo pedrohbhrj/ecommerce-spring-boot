5ce890807e64bf9cc6bae183466c631d2e02835d
pedro <l2pedro157@gmail.com>
2026-04-13 13:16:51 -0300

    add: produto migration

---
+++ src/main/resources/db.migrations/V3__create_produto_table.sql
@@ -0,0 +1,16 @@
+create table produto(
+    id UUID primary key default gen_random_uuid(),
+    nome varchar(50) not null,
+    descricao varchar(300) null,
+    preco DECIMAL(10,2) not null,
+    quantidade_estoque INTEGER not null,
+    img_url varchar(255) null,
+    criado_em timestamp not null not update,
+    atualizado_em timestamp not null,
+    id_vendedor UUID
+
+    CONSTRAINT fk_vendedor
+    FOREIGN KEY id_vendedor
+    REFERENCES usuario(id)
+    ON DELETE CASCADE ALL
+);
