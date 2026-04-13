394a37c55348131a6122664882ffd0c8052110ab
pedro <l2pedro157@gmail.com>
2026-04-13 13:36:41 -0300

    add: item pedido entity e seus relacionamentos

--- src/main/resources/db.migrations/V4_create_pedido_table.sql
+++ src/main/resources/db.migrations/V4_create_pedido_table.sql
@@ -2,6 +2,8 @@ create table pedido(
     id UUID primary key gen_random_uuid(),
     preco_total DECIMAL(10,2) not null,
     status_pedido varchar(30) not null,
+    criado_em timestamp not null default CURRENT_TIMESTAMP,
+    atualizado_em timestamp not null default CURRENT_TIMESTAMP,
     id_cliente UUID
 
     CONSTRAINT fk_cliente
