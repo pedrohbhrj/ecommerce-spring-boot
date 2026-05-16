5459b1f4e16f89666048f6bcb51fd16cce3ba2da
pedro <l2pedro157@gmail.com>
2026-05-16 11:23:51 -0300

    feat: added product entity, auditing properties on user

--- src/main/java/br/com/pedrohbhrj/models/User.java
+++ src/main/java/br/com/pedrohbhrj/models/User.java
@@ -6,10 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -38,6 +41,12 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private String password;
 
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -46,6 +55,7 @@ public class User implements UserDetails {
                 .map(r -> new SimpleGrantedAuthority(r.name()))
                 .collect(Collectors.toSet());
     }
+
 
     @Override
     public String getPassword() {
