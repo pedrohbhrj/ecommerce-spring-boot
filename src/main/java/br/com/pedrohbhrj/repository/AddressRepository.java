4a9850d79cafb1eb870ff5f0157d0f988cd7f5da
pedro <l2pedro157@gmail.com>
2026-05-16 16:00:40 -0300

    feat: add query methods

--- src/main/java/br/com/pedrohbhrj/repository/AddressRepository.java
+++ src/main/java/br/com/pedrohbhrj/repository/AddressRepository.java
@@ -5,6 +5,9 @@ import br.com.pedrohbhrj.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    List<Address> findAllByUserId(Long userId);
 }
