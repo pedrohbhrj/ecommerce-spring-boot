df357a20d42105e6aa0333cf4c3908b40d42d98a
pedro <l2pedro157@gmail.com>
2026-05-16 21:22:20 -0300

    feat: add payment service implements

---
+++ src/main/java/br/com/pedrohbhrj/services/interf/PaymentService.java
@@ -0,0 +1,9 @@
+package br.com.pedrohbhrj.services.interf;
+
+import br.com.pedrohbhrj.DTO.response.PaymentResponse;
+
+public interface PaymentService {
+
+    PaymentResponse processPayment(Long orderId);
+    PaymentResponse findPaymentById(Long orderI);
+}
