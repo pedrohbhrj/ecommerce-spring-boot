fd975adef497941d28d1b99cfab740518b696a88
pedro <l2pedro157@gmail.com>
2026-06-07 11:06:19 -0300

    feat: add exceptions handlers

--- src/main/java/br/com/pedrohbhrj/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/com/pedrohbhrj/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package br.com.pedrohbhrj.exceptions;
 
+import com.stripe.exception.StripeException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -59,7 +62,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(StockLimitExceededException.class)
     public ResponseEntity<CustomErrorDetails<?>> limitExceededExceptionHandler(StockLimitExceededException ex, HttpServletRequest request) {
-        log.warn("Stock limit exceeded route {}: {}",request.getRequestURI(), ex.getMessage());
+        log.warn("Stock limit exceeded route {}: {}", request.getRequestURI(), ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST.value())
                 .body(new CustomErrorDetails<>(
@@ -71,10 +74,49 @@ public class GlobalExceptionHandler {
                 ));
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<CustomErrorDetails<?>> accessDeniedException(AccessDeniedException ex, HttpServletRequest request) {
+
+        log.warn("Access denied route {}: {}", request.getRequestURI(), ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN.value())
+                .body(new CustomErrorDetails<>("Unathorized route.",
+                        LocalDateTime.now(),
+                        request.getRequestURI(),
+                        HttpStatus.FORBIDDEN.value(),
+                        null));
+    }
+
+    @ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class})
+    public ResponseEntity<CustomErrorDetails<?>> illegalExceptionHandler(RuntimeException ex, HttpServletRequest request) {
+        log.warn("Invalid state/argument on route {}: {}", request.getRequestURI(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value())
+                .body(new CustomErrorDetails<>(
+                        ex.getMessage(),
+                        LocalDateTime.now(),
+                        request.getRequestURI(),
+                        HttpStatus.BAD_REQUEST.value(),
+                        null
+                ));
+    }
+
+    @ExceptionHandler(StripeException.class)
+    public ResponseEntity<CustomErrorDetails<?>> stripeExceptionHandler(StripeException ex, HttpServletRequest request) {
+
+        log.warn("Stripe api error on route {}: {}", request.getRequestURI(), ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_GATEWAY.value())
+                .body(new CustomErrorDetails<>("Unathorized route.",
+                        LocalDateTime.now(),
+                        request.getRequestURI(),
+                        HttpStatus.BAD_GATEWAY.value(),
+                        null));
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<CustomErrorDetails<List<CustomFieldErrors>>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex, HttpServletRequest request) {
 
-        log.warn("Method argument not valid route {}: {}", request.getRequestURI(),ex.getMessage());
+        log.warn("Method argument not valid route {}: {}", request.getRequestURI(), ex.getMessage());
 
         List<CustomFieldErrors> list = ex.getBindingResult().getFieldErrors().stream().map(err -> new CustomFieldErrors(err.getField(), err.getDefaultMessage())).toList();
 
@@ -91,6 +133,19 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(customErrorDetails);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<CustomErrorDetails<?>> httpMessageNotReadableExceptionHandler(HttpMessageNotReadableException ex, HttpServletRequest request) {
+        log.warn("Malformed JSON payload on route {}: {}", request.getRequestURI(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value())
+                .body(new CustomErrorDetails<>(
+                        "The body requisition is invalid or has malformed data.",
+                        LocalDateTime.now(),
+                        request.getRequestURI(),
+                        HttpStatus.BAD_REQUEST.value(),
+                        null
+                ));
     }
 
     public record CustomFieldErrors(String field,
